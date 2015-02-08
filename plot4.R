aba40ebc7e4c155075e7f7520b7324eb1fdc9540
Yuliang Wang <yuliangwang86@gmail.com>
2015-02-08 00:10:10 -0800

    Final versions of plots and codes, with minor changes to png4

--- plot4.R
+++ plot4.R
@@ -13,7 +13,7 @@ par(mfcol=c(2,2))
 #same as plot2
 with(electric_power,plot(date_time,Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)"))
 #same as plot3
-with(electric_power,{plot(date_time,Sub_metering_1,type="l",xlab="",ylab="Global Active Power (kilowatts)",col="black")
+with(electric_power,{plot(date_time,Sub_metering_1,type="l",xlab="",ylab="Energy sub metering",col="black")
                      lines(date_time,Sub_metering_2,col="red")
                      lines(date_time,Sub_metering_3,col="blue")
                      legend("topright",lty=1,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"))})
